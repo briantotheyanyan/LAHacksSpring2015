d18a5022d6534eee8b64f3df66802ed38a12125c
briantotheyanyan <sorakeybade@gmail.com>
2015-04-04 19:21:58 -0700

    Bleh

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-		$('head').append(<script src='https://cdn.firebase.com/v0/firebase.js'></script>);
+		var new_script = document.createElement('script');
+		new_script.type = 'text/javascript';
+		new_script.src = './firebase.js';
+		new_script.className = 'MyInjectedScript';
+		document.getElementsByTagName('head')[0].appendChild(new_script);
 		var f = new Firebase("https://swagaliicious.firebaseio.com/");
 		f.on('child_added', function (snapshot) {
 				var message = snapshot.val();
--- scripts/browser_action.js
+++ scripts/browser_action.js
@@ -1,13 +1,22 @@
-Firebase.enableLogging(true);
-var f = new Firebase('https://swagalicious.firebaseio.com/');
-
-window.addEventListener("mouseup", function(event){
-		var line="";
-		line = window.getSelection().toString();
-		console.log(line);
-
-		if(line != ""){
-			f.push({"text": line});
-		}
+$(document).ready(function(){
+		var new_script = document.createElement('script');
+		new_script.type = 'text/javascript';
+		new_script.src = './firebase.js';
+		new_script.className = 'MyInjectedScript';
+		document.getElementsByTagName('head')[0].appendChild(new_script);
+		
+		Firebase.enableLogging(true);
+		var f = new Firebase('https://swagalicious.firebaseio.com/');
+		
+		window.addEventListener("mouseup", function(event){
+				var line="";
+				line = window.getSelection().toString();
+				console.log(line);
+				
+				if(line != ""){
+					f.push({"text": line});
+				}
+			}
+			);
 	}
 	);
