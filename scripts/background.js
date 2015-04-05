1dde87759a4dacf6b405b2505dfc14e848470fc1
briantotheyanyan <sorakeybade@gmail.com>
2015-04-04 18:16:35 -0700

    Refactored firebases code

--- scripts/background.js
+++
@@ -1,7 +0,0 @@
-//$("head").append("<script src='firebase.js'> </script>");
-var swagaliicious = new Firebase("https://swagaliicious.firebaseio.com/");
-
-messagesRef.on('child_added', function (snapshot) {
-    var message = snapshot.val();
-    alert(message.text);
-});
