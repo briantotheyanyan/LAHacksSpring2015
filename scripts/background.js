ee829616dbd575a56c6efc37d7af2c110cbe9587
Brian Zhang <brianzhang@Brians-MacBook-Pro.local>
2015-04-04 17:59:32 -0700

    meh

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,7 @@
-chrome.extension.onMessage.addListener(function(company){
-		console.log(company.name);
-	}
-	);
+//$("head").append("<script src='firebase.js'> </script>");
+var swagaliicious = new Firebase("https://swagaliicious.firebaseio.com/");
+
+messagesRef.on('child_added', function (snapshot) {
+    var message = snapshot.val();
+    alert(message.text);
+});
